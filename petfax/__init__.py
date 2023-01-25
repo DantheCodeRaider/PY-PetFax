8c9e70483310cc16f30b235ce3520366a2493b17
Dan <103788290+DantheCodeRaider@users.noreply.github.com>
2023-01-24 20:41:02 -0600

    Added blueprint and template

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,5 +1,7 @@
 # config   
 from flask import Flask 
+
+    # factory
 def create_app(): 
     app = Flask(__name__)
 
@@ -8,9 +10,14 @@ def create_app():
     def hello(): 
         return 'Hello, this is PetFax!'
 
-# pets index route
-    @app.route('/pets')
-    def pets(): 
-        return 'These are our pets available for adoption!'
+# register pet blueprint 
+    from . import pet
+    app.register_blueprint(pet.bp)
 
+# pets index route
+#    @app.route('/pets')
+#    def pets(): 
+#        return 'These are our pets available for adoption!'
+
+    # return the app 
     return app
