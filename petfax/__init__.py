5efd89394648e2b6f35b587f97d8871754db347e
Dan <103788290+DantheCodeRaider@users.noreply.github.com>
2023-01-26 19:35:02 -0600

    Added SQL connection

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,9 +1,18 @@
 # config   
-from flask import Flask 
+from flask import Flask
+from flask_migrate import Migrate  
 
 # factory
 def create_app(): 
     app = Flask(__name__)
+
+    # database config 
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:%24d8e3k99SA%25@127.0.0.1:5432/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+    from . import models 
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)              
 
 # index route
     @app.route('/')
